8037554eb3c06a2fab801db2089db3cf876859e5
Hadi Nur Wahid <hadiykk99@gmail.com>
2018-12-08 21:33:02 +0700

    add mlab

--- Entertain/configs/endpoint.js
+++ Entertain/configs/endpoint.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     endPoint: {
-        movieEndpoint: 'http://localhost:3001',
-        tvEndpoint: 'http://localhost:3002'
+        movieEndpoint: process.env.MOVIE_END_POINT || 'http://localhost:3001',
+        tvEndpoint: process.env.TV_END_POINT || 'http://localhost:3002'
     }
 } 
